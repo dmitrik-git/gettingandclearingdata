a7b9fdac3ea4773f716ef096db7aa21b62c80943
Dmitri K <dmitri.kovagin@gmail.com>
2020-12-04 17:08:13 +0200

    Question 3

--- week3quiz.R
+++ week3quiz.R
@@ -1,3 +1,12 @@
+# Question 1
+# Create a logical vector that identifies the households on greater than 10 acres who sold more than $10,000 worth of agriculture products. 
+# Assign that logical vector to the variable agricultureLogical. 
+# Apply the which() function like this to identify the rows of the data frame where the logical vector is TRUE.
+
+# which(agricultureLogical)
+
+# What are the first 3 values that result?
+
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
 if (!file.exists("data")) {
   dir.create("data")
@@ -8,4 +17,43 @@ data <- read.csv("./data/2006survey.csv")
 
 agricultureLogical <- data$ACR == 3 & data$AGS == 6
 which(agricultureLogical)
+
+# Question 2
+# Using the jpeg package read in the following picture of your instructor into R
+
+install.packages("jpeg")
+library(jpeg)
+
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"
+if (!file.exists("data")) {
+  dir.create("data")
+} 
+
+download.file(fileURL, destfile = "./data/jeff.jpg", mode = "wb")
+data <- readJPEG("./data/jeff.jpg", native = TRUE)
+quantile (data, probs = c(0.3, 0.8))
+
+# Question 3
+
+# Match the data based on the country shortcode. 
+# How many of the IDs match? Sort the data frame in descending order by GDP rank (so United States is last). 
+# What is the 13th country in the resulting data frame?
+
+if (!file.exists("data")) {
+  dir.create("data")
+} 
+fileURL1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+fileURL2 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+
+download.file(fileURL1, destfile = "./data/GDP.csv", mode = "wb")
+download.file(fileURL2, destfile = "./data/Country.csv")
+dateDownloaded <- date()
+gdps <- read.csv("./data/GDP.csv", blank.lines.skip = TRUE, skip = 4)
+countries <- read.csv("./data/Country.csv")
+mergedData <- merge (gdps, countries, by.x = "X", by.y = "CountryCode", all=TRUE)
+mergedData <- mergedData[order(mergedData$X.1, decreasing = TRUE),]
+
+
+
+
 
