01873ecfbd5415860b3ca73360e4b7391fddcef9
Dmitri K <dmitri.kovagin@gmail.com>
2020-12-05 12:57:59 +0200

    Question 5

--- week3quiz.R
+++ week3quiz.R
@@ -70,6 +70,15 @@ library(reshape2)
 meltedData <- melt (arrangedData, id = "Income.Group", measure.vars = "Rank")
 dcast(meltedData, Income.Group ~ variable, mean)
 
+# Question 5
+# Cut the GDP ranking into 5 separate quantile groups. Make a table versus Income.Group. 
+# How many countries are Lower middle income but among the 38 nations with highest GDP?
+
+qntl <- quantile (arrangedData$Rank, probs = c(.2, .4, .6, .8, 1))
+
+#adding a new variable whith value equal to the quantile slice
+arrangedData$groupSlice <- cut(arrangedData$Rank, breaks = qntl)
+table(arrangedData$groupSlice, arrangedData$Income.Group)
 
 
 
